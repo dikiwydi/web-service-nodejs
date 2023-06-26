a96659172534565719e29cc3fa6d61bc2a81f736
dikiwydi <dikiwahyudi268@gmail.com>
2023-06-26 15:04:57 +0700

    response header and response body

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("X-Powered-By", "NodeJS");
 
   const { method, url } = request;
 
   if (url === "/") {
     if (method === "GET") {
       response.statusCode = 400;
-      response.end("<h1>halo ini adalah halaman get</h1>");
+      response.end(
+        JSON.stringify({
+          message: "halo ini adalah halaman get",
+        })
+      );
     } else {
       response.statusCode = 400;
-      response.end(`method ${method} yang kamu kasih salah `);
+      response.end(
+        JSON.stringify({
+          message: `halaman tidak bisa di akses dengan ${method} request`,
+        })
+      );
     }
   } else if (url === "/about") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end(`<h1>halo ini adalah halaman get</h1>`);
+      response.end(
+        JSON.stringify({
+          message: `halo ini adalah halaman about`,
+        })
+      );
     } else if (method === "POST") {
       let body = [];
 
@@ -25,12 +38,20 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { nama } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>halo ${nama}`);
+        response.end(
+          JSON.stringify({
+            message: `halo${nama} saat ini anda berada halaman about`,
+          })
+        );
       });
     }
   } else {
     response.statusCode = 404;
-    response.end(`<h1>maaf url yang kamu masukan ${url} salah</h1>`);
+    response.end(
+      JSON.stringify({
+        message: `maaf url yang kamu masukan ${url} salah`,
+      })
+    );
   }
 };
 
