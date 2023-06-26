c20bf6ee86545eb890c0da9cd6b4af03d3c608d9
dikiwydi <dikiwahyudi268@gmail.com>
2023-06-26 14:28:11 +0700

    routing request

--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const http = require("http");
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.setStatusCode = 200;
-  const { method } = request;
-  if (method === "GET") {
-    response.end("<h1>halo get </h1>");
-  }
-  if (method === "POST") {
-    let body = [];
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>halo ${name}</h1>`);
-    });
+  const { method, url } = request;
+
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>halo ini adalah halaman get</h1>");
+    } else {
+      response.end(`method ${method} yang kamu kasih salah `);
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end(`<h1>halo ini adalah halaman get</h1>`);
+    } else if (method === "POST") {
+      let body = [];
+
+      request.on("data", (chunk) => body.push(chunk));
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { nama } = JSON.parse(body);
+        response.end(`<h1>halo ${nama}`);
+      });
+    }
+  } else {
+    response.end(`<h1>maaf url yang kamu masukan ${url} salah</h1>`);
   }
 };
 
