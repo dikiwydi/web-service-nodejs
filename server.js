248de2ac8fe226cb84877943a28ecc430727d0cd
dikiwydi <dikiwahyudi268@gmail.com>
2023-06-26 14:42:40 +0700

    status response

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-  response.setStatusCode = 200;
 
   const { method, url } = request;
 
   if (url === "/") {
     if (method === "GET") {
+      response.statusCode = 400;
       response.end("<h1>halo ini adalah halaman get</h1>");
     } else {
+      response.statusCode = 400;
       response.end(`method ${method} yang kamu kasih salah `);
     }
   } else if (url === "/about") {
     if (method === "GET") {
+      response.statusCode = 200;
       response.end(`<h1>halo ini adalah halaman get</h1>`);
     } else if (method === "POST") {
       let body = [];
@@ -22,10 +24,12 @@ const requestListener = (request, response) => {
       request.on("end", () => {
         body = Buffer.concat(body).toString();
         const { nama } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>halo ${nama}`);
       });
     }
   } else {
+    response.statusCode = 404;
     response.end(`<h1>maaf url yang kamu masukan ${url} salah</h1>`);
   }
 };
